aa6b3f61fdf8a4d7daf5ec527ea18fef441eb934
Sunaina <sunainahegnekar@gmail.com>
2024-11-22 11:01:04 +0530

    Added mobile responsiveness

--- script.js
+++ script.js
@@ -17,4 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   
 
+  const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.navbar-links');
+
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navLinks.classList.toggle('active');
+    });
+
+    // Close menu when clicking a link
+    document.querySelectorAll('.navbar-links li a').forEach(link => {
+      link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+      });
+    });
+
   
